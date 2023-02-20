5d04a66dd2568b9b75665986e9b91db5e0bea693
Neeraj Modi <neerajmodimit2010@gmail.com>
2023-02-20 12:18:52 +0530

    added configuration fields before lunch

--- src/app/seller-form/seller-form.component.ts
+++ src/app/seller-form/seller-form.component.ts
@@ -17,10 +17,12 @@ export class SellerFormComponent {
     yearOfManufacture:[''],
     yearOfPurchase:[''],
     configuration:this.fb.group({
-      ram:[''],
       processor:[''],
+      ram:[''],
+      ramType:[''],
       storage:[''],
-      stroageType:[''],
+      storageHDD:[''],
+      storageSSD:[''],
       graphicCard:[''],
       dvd:[''],
       charger:[''],
@@ -34,4 +36,8 @@ export class SellerFormComponent {
   })
 
   manufacturer:string[]=['Lenovo','HP','Dell','Apple','Acer','Asus','Samsung','msi']
+  hdd:string[]=['128GB','256GB','500GB','1TB','2TB','4TB']
+  ssd:string[]=['128GB','256GB','500GB','1TB','2TB']
+  ramStorage:string[]=['1GB','2GB','4GB','8GB','16GB']
+  ramdrrList:string[]=['DDR2-400','DDR2-533','DDR2-667','DDR2-800','DDR2-1000','DDR3-800','DDR3-1066','DDR3-1333','DDR3-1600','DDR4-2400','DDR4-2666','DDR4-2933','DDR4-3000','DDR4-3200','DDR4-3600','DDR4-4000','DDR4-4400']
 }
